59d4023461fab3523468a7a536eeb468610f7fc3
Jordan Harris <jordanharris1175@gmail.com>
2024-03-18 01:17:31 -0400

    Add shadcn/ui and a theme toggle in header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import cx from "classnames";
-import { sfPro, inter } from "./fonts";
+import { cn } from "@/lib/utils";
+import { sfPro, fontSans } from "./fonts";
 import Nav from "@/components/layout/nav";
 import Footer from "@/components/layout/footer";
 import { Suspense } from "react";
 import { Metadata, Viewport } from "next";
+import { ThemeProvider } from "@/components/home/theme-provider";
 
 export const metadata: Metadata = {
-  title: "Precedent - Building blocks for your Next.js project",
-  description:
-    "Precedent is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
-  metadataBase: new URL("https://precedent.dev"),
+  title: "Prophet Message",
+  description: "New message app built with Next.js, Tailwind, and Prisma.",
+  // metadataBase: new URL("https://precedent.dev"),
 };
 
 export const viewport: Viewport = {
   themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "cyan" },
+    { media: "(prefers-color-scheme: light)", color: "" },
     { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
 };
@@ -28,15 +28,24 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={cx(sfPro.variable, inter.variable)}>
-        <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
-        <Suspense fallback="...">
-          <Nav />
-        </Suspense>
-        <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Suspense fallback="...">
+            <Nav />
+          </Suspense>
           {children}
-        </main>
-        <Footer />
+          <Footer />
+        </ThemeProvider>
         <Analytics />
       </body>
     </html>
--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -6,6 +6,8 @@ import useScroll from "@/lib/hooks/use-scroll";
 import { useSignInModal } from "./sign-in-modal";
 import UserDropdown from "./user-dropdown";
 import { Session } from "next-auth";
+import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "../home/ThemeToggle";
 
 export default function NavBar({ session }: { session: Session | null }) {
   const { SignInModal, setShowSignInModal } = useSignInModal();
@@ -15,34 +17,23 @@ export default function NavBar({ session }: { session: Session | null }) {
     <>
       <SignInModal />
       <div
-        className={`fixed top-0 w-full flex justify-center ${
+        className={`fixed top-0 flex w-full justify-center ${
           scrolled
             ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
             : "bg-white/0"
         } z-30 transition-all`}
       >
-        <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
-          <Link href="/" className="flex items-center font-display text-2xl">
-            <Image
-              src="/logo.png"
-              alt="Precedent logo"
-              width="30"
-              height="30"
-              className="mr-2 rounded-sm"
-            ></Image>
-            <p>Precedent</p>
+        <div className="mx-5 flex h-16 w-full max-w-screen-xl items-center justify-between">
+          <Link href="/" className="font-display flex items-center text-2xl">
+            <p>Prophet Message</p>
           </Link>
-          <div>
+          <div className="flex items-center gap-2">
             {session ? (
               <UserDropdown session={session} />
             ) : (
-              <button
-                className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
-                onClick={() => setShowSignInModal(true)}
-              >
-                Sign In
-              </button>
+              <Button onClick={() => setShowSignInModal(true)}>Sign In</Button>
             )}
+            <ThemeToggle />
           </div>
         </div>
       </div>
